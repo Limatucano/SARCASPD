d9190355e0b22b0a4d8ea89c37369c8c76a39ed5
matheus lima <matheusmt31@gmail.com>
2022-06-01 13:25:04 -0300

    adicionado campos restantes no json e refatoracao para comportar categoria

--- app/src/main/java/com/br/faeterj/paracambi/sarcaspd/data/model/Field.kt
+++ app/src/main/java/com/br/faeterj/paracambi/sarcaspd/data/model/Field.kt
@@ -1,18 +1,20 @@
 package com.br.faeterj.paracambi.sarcaspd.data.model
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-import kotlinx.android.parcel.RawValue
+import kotlinx.parcelize.Parcelize
+import kotlinx.parcelize.RawValue
 
 @Parcelize
 data class Field(
-    val title: String,
-    val fieldType: String,
-    val hint: String?,
-    val inputType: String?,
-    val id: String,
-    val min: Int?,
-    val max: Int?,
+    val title: String = "",
+    val fieldType: String = "",
+    val hint: String? = null,
+    val inputType: String? = null,
+    val id: String = "",
+    val min: Int? = null,
+    val max: Int? = null,
     val defaultValue: @RawValue Any? = null,
-    val method: String?
+    val method: String? = null,
+    val category: String? = null,
+    val fields : List<Field>? = null
 ) : Parcelable
--- app/src/main/java/com/br/faeterj/paracambi/sarcaspd/view/FormFragment.kt
+++ app/src/main/java/com/br/faeterj/paracambi/sarcaspd/view/FormFragment.kt
@@ -12,13 +12,12 @@ import androidx.lifecycle.ViewModelProvider
 import com.br.faeterj.paracambi.sarcaspd.R
 import com.br.faeterj.paracambi.sarcaspd.data.fields.SelectField
 import com.br.faeterj.paracambi.sarcaspd.data.fields.TextField
+import com.br.faeterj.paracambi.sarcaspd.data.model.Field
 import com.br.faeterj.paracambi.sarcaspd.data.model.Form
 import com.br.faeterj.paracambi.sarcaspd.databinding.FragmentFormBinding
 import com.br.faeterj.paracambi.sarcaspd.domain.FirstBlock
 import com.br.faeterj.paracambi.sarcaspd.viewModel.FormViewModel
 import com.br.faeterj.paracambi.sarcaspd.viewModel.FormViewModelFactory
-import java.lang.reflect.Modifier
-import kotlin.reflect.KFunction
 import kotlin.reflect.jvm.javaMethod
 import kotlin.reflect.jvm.kotlinFunction
 
@@ -29,6 +28,7 @@ class FormFragment : Fragment() {
     private lateinit var viewModel: FormViewModel
     private lateinit var form: Form
     private var adapter: ArrayAdapter<*>? = null
+    private val fieldsCreated: MutableList<View> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,44 +51,61 @@ class FormFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         //TODO: Inject blocks inside factory
-        val fieldsCreated: MutableList<View> = mutableListOf()
+
 
         viewModel = ViewModelProvider(this, FormViewModelFactory(FirstBlock(requireContext()))).get(
             FormViewModel::class.java
         )
 
-        for (field in form.block) {
+        scrollingList(form.block)
 
-            val viewTitle = TextView(requireContext())
-            lateinit var viewField: View
-            viewTitle.text = field.title
-            field.method?.let { method ->
-                getFunctionByName(method)
+    }
+
+    private fun scrollingList(fields: List<Field>){
+
+        for (field in fields) {
+
+            if(field.category != null && !field.fields.isNullOrEmpty()){
+                val categoryTitle = TextView(requireContext())
+                categoryTitle.text = field.category
+                viewBinding.parentLayout.addView(categoryTitle)
+                scrollingList(field.fields)
+                continue
             }
-
-            when (field.fieldType) {
-                "SELECT" -> {
-                    viewField = SelectField(requireContext(), adapter, field).getField()
-                }
-                "TEXT" -> {
-                    viewField = TextField(requireContext(), field).getField()
-                }
-            }
-
-            if (viewField.parent != null) {
-                (viewField.parent as ViewGroup).removeView(viewField)
-            }
-
-            adapter = null
-            fieldsCreated.add(viewTitle)
-            fieldsCreated.add(viewField)
-            viewBinding.parentLayout.addView(viewTitle)
-            viewBinding.parentLayout.addView(viewField)
+            buildFields(field)
         }
 
     }
 
-    fun getFunctionByName(name: String) {
+    private fun buildFields(field : Field){
+        val viewTitle = TextView(requireContext())
+        lateinit var viewField: View
+        viewTitle.text = field.title
+        field.method?.let { method ->
+            getFunctionByName(method)
+        }
+
+        when (field.fieldType) {
+            "SELECT" -> {
+                viewField = SelectField(requireContext(), adapter, field).getField()
+            }
+            "TEXT" -> {
+                viewField = TextField(requireContext(), field).getField()
+            }
+        }
+
+        if (viewField.parent != null) {
+            (viewField.parent as ViewGroup).removeView(viewField)
+        }
+
+        adapter = null
+        fieldsCreated.add(viewTitle)
+        fieldsCreated.add(viewField)
+        viewBinding.parentLayout.addView(viewTitle)
+        viewBinding.parentLayout.addView(viewField)
+    }
+
+    private fun getFunctionByName(name: String) {
         val selfRef = ::getFunctionByName
         val currentClass = selfRef.javaMethod?.declaringClass
         val classFunction = currentClass?.classLoader?.loadClass(this.javaClass.name)
@@ -108,7 +125,52 @@ class FormFragment : Fragment() {
             getString(R.string.escavado),
             getString(R.string.perfurado)
         )
-        adapter =
-            ArrayAdapter(requireContext(), R.layout.simple_dropdown_item_1line, artesianWellTypes)
+        adapter = ArrayAdapter(requireContext(), R.layout.simple_dropdown_item_1line, artesianWellTypes)
+    }
+
+    fun buildNearArtesianWellField(){
+        val position = listOf(
+            getString(R.string.nao_possui),
+            getString(R.string.acima_boca),
+            getString(R.string.mesmo_nivel),
+            getString(R.string.abaixo_boca)
+        )
+        adapter = ArrayAdapter(requireContext(), R.layout.simple_dropdown_item_1line, position)
+    }
+
+    fun buildTopInclinationField(){
+        val inclinations = listOf(
+            getString(R.string.inclinada),
+            getString(R.string.pouco_inclinada),
+            getString(R.string.plana),
+        )
+        adapter = ArrayAdapter(requireContext(), R.layout.simple_dropdown_item_1line, inclinations)
+    }
+
+    fun buildPollutingSourceField(){
+        val inclinations = listOf(
+            getString(R.string.fonte_um),
+            getString(R.string.fonte_dois),
+            getString(R.string.fonte_tres),
+        )
+        adapter = ArrayAdapter(requireContext(), R.layout.simple_dropdown_item_1line, inclinations)
+    }
+
+    fun buildSoilField(){
+        val inclinations = listOf(
+            getString(R.string.argila),
+            getString(R.string.silte),
+            getString(R.string.areia),
+        )
+        adapter = ArrayAdapter(requireContext(), R.layout.simple_dropdown_item_1line, inclinations)
+    }
+
+    fun buildDepthField(){
+        val inclinations = listOf(
+            getString(R.string.profundidade_um),
+            getString(R.string.profundidade_dois),
+            getString(R.string.profundidade_tres),
+        )
+        adapter = ArrayAdapter(requireContext(), R.layout.simple_dropdown_item_1line, inclinations)
     }
 }
